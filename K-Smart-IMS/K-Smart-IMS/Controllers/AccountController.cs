0a7e31e18e43a359e82d4025bb18719cca1d6212
codytran1994 <cody.tran@go.sfcollege.edu>
2022-03-24 20:20:11 -0400

    Added account login features
    Added rudimentary account features

---
+++ K-Smart-IMS/K-Smart-IMS/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using K_Smart_IMS.Models;
+
+namespace K_Smart_IMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userMngr,
+            SignInManager<User> signInMngr)
+        {
+            userManager = userMngr;
+            signInManager = signInMngr;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new K_Smart_IMS.Models.User { UserName = model.Username };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent : false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors) 
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult LogIn(string returnURL = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnURL };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {                
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, isPersistent: model.RememberMe, 
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && 
+                        Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid username/password.");
+            return View(model);
+        }
+
+        public ViewResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ K-Smart-IMS/K-Smart-IMS/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace K_Smart_IMS.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public bool RememberMe { get; set; }
+
+    }
+}
