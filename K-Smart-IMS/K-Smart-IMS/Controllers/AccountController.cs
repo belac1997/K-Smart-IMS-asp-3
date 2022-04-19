bdbb92c812d2ad2d70bd7690e8cd49b8427fd922
codytran1994 <cody.tran@go.sfcollege.edu>
2022-04-18 20:32:34 -0400

    Revert "Update AccountController.cs"
    This reverts commit 0ff152a32c26423f4899935fd7db1671bf76e31b.

--- K-Smart-IMS/K-Smart-IMS/Controllers/AccountController.cs
+++ K-Smart-IMS/K-Smart-IMS/Controllers/AccountController.cs
@@ -60,18 +60,25 @@ namespace K_Smart_IMS.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [HttpGet]
+        public IActionResult LogIn(string returnURL = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnURL };
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> LogIn(LoginViewModel model)
         {
             if (ModelState.IsValid)
-            {
+            {                
                 var result = await signInManager.PasswordSignInAsync(
-                    model.Username, model.Password, isPersistent: model.RememberMe,
-                    lockoutOnFailure: true);
+                    model.Username, model.Password, isPersistent: model.RememberMe, 
+                    lockoutOnFailure: false);
 
-                if (result.Succeeded)//succesfull log in
+                if (result.Succeeded)
                 {
-                    if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && 
                         Url.IsLocalUrl(model.ReturnUrl))
                     {
                         return Redirect(model.ReturnUrl);
@@ -81,15 +88,8 @@ namespace K_Smart_IMS.Controllers
                         return RedirectToAction("Index", "Home");
                     }
                 }
-                if (result.IsLockedOut)//Lock out test.
-                {
-                    ModelState.AddModelError("", "Account is Locked out.");
-                }
-                else//wrong password test
-                {
-                    ModelState.AddModelError("", "Invalid username/password.");
-                }
             }
+            ModelState.AddModelError("", "Invalid username/password.");
             return View(model);
         }
 
